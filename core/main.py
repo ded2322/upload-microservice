ce91496b6562e13149fa6b47818090b51acac3a4
ded2322 <151318767+ded2322@users.noreply.github.com>
2024-06-07 11:23:00 +0700

    v.1.0

--- core/main.py
+++ core/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.routers.router import router
 
@@ -11,3 +12,13 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="/core/static"), "static")
 
 app.include_router(router)
+
+origins = ["localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
